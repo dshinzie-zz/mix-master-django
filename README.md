355e69385d2bffa73caf9694cd0b0b66dc41c75c
Daniel Shin <shin.daniel.d@gmail.com>
2016-11-26 21:52:12 -0700

    trying to be a django bro

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a rewrite of Mix Master from Ruby and Rails to Python and Django. Testing is rewritten from RSpec to Lettuce.
---
+++ mix_master/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class MixMasterConfig(AppConfig):
+    name = 'mix_master'
