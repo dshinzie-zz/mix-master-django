92b3af9cd86304cf4fe014f793acf48bf006a2ba
Daniel Shin <shin.daniel.d@gmail.com>
2017-03-11 15:47:52 -0700

    songs list and links created

--- mix_master/urls.py
+++ mix_master/urls.py
@@ -4,11 +4,16 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    #artists
     url(r'^artists/$', views.artist_list, name='artist_list'),
     url(r'^artists/(?P<pk>\d+)/$', views.artist_detail, name='artist_detail'),
     url(r'^artists/new/$', views.artist_new, name='artist_new'),
     # url(r'^artists/(?P<pk>\d+)/edit/$', views.artist_edit, name='artist_edit'),
 
-    # songs
+    # artist songs
     url(r'^artists/(?P<pk>\d+)/songs/new/$', views.artist_song_new, name='artist_song_new'),
+
+    #songs
+    url(r'^songs/$', views.song_list, name='song_list'),
+    url(r'^songs/(?P<pk>\d+)/$', views.song_detail, name='song_detail'),
 ]
--- mix_master/views.py
+++ mix_master/views.py
@@ -20,19 +20,29 @@ def artist_new(request):
             artist = form.save(commit=False)
             artist.published_date = timezone.now()
             artist.save()
-            return redirect('artist_index')
+            return redirect('artist_list')
     else:
         form = ArtistForm()
     return render(request, 'artists/new.html', {'form': form})
 
 def artist_song_new(request, pk):
+    artist = get_object_or_404(Artist, pk=pk)
     if(request.method) == 'POST':
         form = SongForm(request.POST)
         if form.is_valid():
             song = form.save(commit=False)
             song.published_date = timezone.now()
+            song.artist = artist
             song.save()
-            return redirect('artist_index')
+            return redirect('artist_list')
     else:
         form = SongForm()
-    return render(request, 'artists/songs/new.html')
+    return render(request, 'artists/songs/new.html', {'form': form, 'artist': artist})
+
+def song_list(request):
+    songs = Song.objects.all()
+    return render(request, 'songs/list.html', {'songs': songs})
+
+def song_detail(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    return render(request, 'songs/detail.html', {'song': song})
